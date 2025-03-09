969f34863785ec2c3532bf1c5da97353212a1827
HotHat <luyuhux@gmail.com>
2025-03-09 22:06:22 +0800

    add bytebuddy example

--- mybatis-crud-bean/src/test/java/com/lyhux/mybatiscrud/bean/ByteBuddyTest.java
+++ mybatis-crud-bean/src/test/java/com/lyhux/mybatiscrud/bean/ByteBuddyTest.java
@@ -9,8 +9,12 @@ import net.bytebuddy.matcher.ElementMatchers;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
@@ -48,8 +52,7 @@ public class ByteBuddyTest {
     }
 
     @Test
-    public void testAddField() throws InstantiationException, IllegalAccessException, NoSuchFieldException {
-        /*
+    public void testAddField() throws InstantiationException, IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
         DynamicType.Unloaded<Object> dynamicType = new ByteBuddy()
             .subclass(Object.class)
             .defineField("dynamicIntField", int.class, net.bytebuddy.description.modifier.Visibility.PUBLIC)
@@ -58,8 +61,8 @@ public class ByteBuddyTest {
             .intercept(FieldAccessor.ofField("dynamicStringField"))
             .make();
 
-        Class<?> loadedClass = dynamicType.load(ByteBuddyAddField.class.getClassLoader()).getLoaded();
-        Object instance = loadedClass.newInstance();
+        Class<?> loadedClass = dynamicType.load(ByteBuddyTest.class.getClassLoader()).getLoaded();
+        Object instance = loadedClass.getDeclaredConstructor().newInstance();
 
         Field intField = loadedClass.getField("dynamicIntField");
         intField.setInt(instance, 42);
@@ -70,7 +73,72 @@ public class ByteBuddyTest {
         System.out.println("Dynamic Int Field: " + intField.getInt(instance));
         System.out.println("Dynamic String Field: " + stringField.get(instance));
         System.out.println("toString method output: " + instance.toString());
+    }
 
-         */
+    public static <T> Class<? extends T> addSetterInterceptor(Class<T> beanClass, Object interceptor) {
+        return new ByteBuddy()
+            .subclass(beanClass)
+            .method(ElementMatchers.isSetter())
+            .intercept(MethodDelegation.to(interceptor))
+            .make()
+            .load(beanClass.getClassLoader())
+            .getLoaded();
+    }
+
+    @Test
+    public void testSetter() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        GenericSetterInterceptor interceptor = new GenericSetterInterceptor();
+        Class<? extends MyBean> beanClassWithInterceptor = addSetterInterceptor(MyBean.class, interceptor);
+        MyBean bean = beanClassWithInterceptor.getDeclaredConstructor().newInstance();
+
+        bean.setName("Alice");
+        bean.setAge(28);
+
+        System.out.println("Name: " + bean.getName());
+        System.out.println("Age: " + bean.getAge());
+
+        // GenericSetterInterceptor interceptor2 = new GenericSetterInterceptor();
+        // Class<? extends MyBean> beanClassWithInterceptor2 = addSetterInterceptor(MyBean.class, interceptor2);
+        // MyBean bean2 = beanClassWithInterceptor2.getDeclaredConstructor().newInstance();
+        //
+        // bean2.setName("Alice2");
+        // bean2.setAge(38);
+        //
+        // System.out.println("Name: " + bean2.getName());
+        // System.out.println("Age: " + bean2.getAge());
+    }
+    @Test
+    public void testAddFieldAddSetter() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        GenericSetterInterceptor interceptor = new GenericSetterInterceptor();
+        Class<? extends MyBean> beanClassWithInterceptor = addSetterInterceptor(MyBean.class, interceptor);
+
+        Class<? extends MyBean> dynamicType = new ByteBuddy()
+            .subclass(MyBean.class)
+            .defineField("keymap$original", Map.class, net.bytebuddy.description.modifier.Visibility.PUBLIC)
+            // .implement(Map.class)
+            // .intercept(FieldAccessor.ofField("keymap$original"))
+            .method(ElementMatchers.isSetter())
+            .intercept(MethodDelegation.to(interceptor))
+            .make()
+            .load(MyBean.class.getClassLoader())
+            .getLoaded();
+
+        MyBean instance = dynamicType.getDeclaredConstructor().newInstance();
+
+        Field mapField = dynamicType.getDeclaredField("keymap$original");
+        mapField.setAccessible(true);
+        Map<String, Object> map1;
+        map1 = new HashMap<>();
+        map1.put("key1", "value1");
+        map1.put("key2", 123);
+        mapField.set(instance, map1);
+
+        instance.setName("Alice");
+        instance.setAge(28);
+        System.out.println("Name: " + instance.getName());
+        System.out.println("Age: " + instance.getAge());
+
+        System.out.println("keymap: " + instance.getClass().getField("keymap$original").get(instance));
+
     }
 }
